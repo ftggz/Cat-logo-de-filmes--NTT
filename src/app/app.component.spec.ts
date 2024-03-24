2b2ff449a6abfeb269734a12ac0f7686069a03d1
ftggz <fernando.contato12310.@gmail.com>
2024-03-24 17:46:59 -0300

    NTT-Movies

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ntt-movie'`, () => {
+  it(`should have as title 'showtime'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ntt-movie');
+    expect(app.title).toEqual('showtime');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ntt-movie app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('showtime app is running!');
   });
 });
